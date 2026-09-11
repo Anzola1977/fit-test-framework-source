b06ae811377fafdeece72d592bf970b8b0c4cf5b
Andrei Zola <86254021+Anzola1977@users.noreply.github.com>
2026-09-11 07:26:29 +0200

    Update HilfsmittelCaseRepository.java

--- src/main/java/de/fit/framework/persistence/repository/HilfsmittelCaseRepository.java
+++ src/main/java/de/fit/framework/persistence/repository/HilfsmittelCaseRepository.java
@@ -1,3 +1,3 @@
 package de.fit.framework.persistence.repository;
-import de.fit.framework.persistence.entity.HilfsmittelCaseEntity;import org.springframework.data.jpa.repository.*;import java.util.*;
-public interface HilfsmittelCaseRepository extends JpaRepository<HilfsmittelCaseEntity,Long>{@EntityGraph(attributePaths={"positions","positions.appointments"})Optional<HilfsmittelCaseEntity> findDetailedById(Long id);}
+import de.fit.framework.persistence.entity.HilfsmittelCaseEntity;import org.springframework.data.jpa.repository.JpaRepository;
+public interface HilfsmittelCaseRepository extends JpaRepository<HilfsmittelCaseEntity,Long>{}
