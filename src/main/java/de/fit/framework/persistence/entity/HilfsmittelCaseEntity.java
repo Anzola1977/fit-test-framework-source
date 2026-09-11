7c3146a64c435f51a88f768e070917ec3a448552
Andrei Zola <86254021+Anzola1977@users.noreply.github.com>
2026-09-11 07:36:15 +0200

    Update HilfsmittelCaseEntity.java

--- src/main/java/de/fit/framework/persistence/entity/HilfsmittelCaseEntity.java
+++ src/main/java/de/fit/framework/persistence/entity/HilfsmittelCaseEntity.java
@@ -1,3 +1,52 @@
 package de.fit.framework.persistence.entity;
-import de.fit.framework.api.Gender;import jakarta.persistence.*;import java.time.*;import java.util.*;
-@Entity @Table(name="hilfsmittel_case") public class HilfsmittelCaseEntity{@Id @GeneratedValue(strategy=GenerationType.IDENTITY)private Long id;@Enumerated(EnumType.STRING)private Gender gender;private int age;private LocalDate prescribedOn;private boolean coPaymentExempt;@OneToMany(mappedBy="root",cascade=CascadeType.ALL)private final List<PositionEntity> positions=new ArrayList<>();protected HilfsmittelCaseEntity(){}public HilfsmittelCaseEntity(Gender g,int a,LocalDate d,boolean e){gender=g;age=a;prescribedOn=d;coPaymentExempt=e;}public void add(PositionEntity p){positions.add(p);p.attach(this);}public Long getId(){return id;}public Gender getGender(){return gender;}public int getAge(){return age;}public LocalDate getPrescribedOn(){return prescribedOn;}public boolean isCoPaymentExempt(){return coPaymentExempt;}public List<PositionEntity> getPositions(){return positions;}}
+
+import de.fit.framework.api.Gender;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "hilfsmittel_case")
+public class HilfsmittelCaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private int age;
+    private LocalDate prescribedOn;
+    private boolean coPaymentExempt;
+
+    @OneToMany(mappedBy = "root", cascade = CascadeType.ALL)
+    private final List<PositionEntity> positions = new ArrayList<>();
+
+    protected HilfsmittelCaseEntity() {
+    }
+
+    public HilfsmittelCaseEntity(
+            Gender gender,
+            int age,
+            LocalDate prescribedOn,
+            boolean coPaymentExempt) {
+        this.gender = gender;
+        this.age = age;
+        this.prescribedOn = prescribedOn;
+        this.coPaymentExempt = coPaymentExempt;
+    }
+
+    public void add(PositionEntity position) {
+        positions.add(position);
+        position.attach(this);
+    }
+
+    public Long getId() { return id; }
+    public Gender getGender() { return gender; }
+    public int getAge() { return age; }
+    public LocalDate getPrescribedOn() { return prescribedOn; }
+    public boolean isCoPaymentExempt() { return coPaymentExempt; }
+    public List<PositionEntity> getPositions() { return positions; }
+}
