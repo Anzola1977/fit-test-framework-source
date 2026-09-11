589ba6b80d894288b0d4c859469b9914d2d0b37b
Andrei Zola <86254021+Anzola1977@users.noreply.github.com>
2026-09-11 07:35:39 +0200

    Update PositionData.java

--- src/main/java/de/fit/framework/domain/PositionData.java
+++ src/main/java/de/fit/framework/domain/PositionData.java
@@ -1,3 +1,15 @@
 package de.fit.framework.domain;
-import java.time.LocalDate;import java.util.*;
-public record PositionData(int number,LocalDate serviceFrom,LocalDate serviceTo,List<TerminData> appointments){public PositionData{appointments=List.copyOf(appointments);}}
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record PositionData(
+        int number,
+        LocalDate serviceFrom,
+        LocalDate serviceTo,
+        List<TerminData> appointments) {
+
+    public PositionData {
+        appointments = List.copyOf(appointments);
+    }
+}
